a3d9a8887da44d20ec4ac331356e7dd93eb318b9
walace <walacefex01@gmail.com>
2020-11-12 02:02:02 -0300

    Install add express e configurando eslint e prettier

---
+++ modulo02/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'linebreak-style': 0,
+    'class-methods-use-this': 'off',
+    'no-param-reassing': 'off',
+    camelcase: 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+  },
+};
